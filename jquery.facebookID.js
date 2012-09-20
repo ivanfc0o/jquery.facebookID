070b222a36cd6ece8ffc239bd439e084addae8f4
ivanfc0o <ivanfc0o@gmail.com>
2012-09-19 22:59:09 -0700

    Pushing..

---
+++ jquery.facebookID.js
@@ -0,0 +1,29 @@
+/*
+ *  Project: facebookID
+ *  Description: Get basic info from an user without Apikey or App
+ *  Author: @ivanfc0o
+ */
+
+;(function ( $, window, undefined ) {
+    
+// Create the defaults once
+var pluginName = 'facebookID';
+
+    // plugin __constructor
+    var plugin = function(user, avatar, callback){
+        var avatar = avatar||[50,50];
+        var emailRegExp = /^\w[\+\.\w-]*@facebook.com$/i,
+              user = emailRegExp.test(user)?user.split("@")[0]:user,
+              result = {},
+              data = $.getJSON("https://graph.facebook.com/"+user, null, function(d){
+                                      result.name = d.name;
+                                      result.avatar = "http://graph.facebook.com/"+d.username+"/picture?width="+avatar[0]+"&height="+avatar[1];
+                                      result.url = d.link||"http://fb.com/"+d.username;
+                                      if((typeof callback)==="function"){ callback(result); }
+                          });
+              return result;
+    };
+
+$[pluginName] = plugin;
+
+}(jQuery, window));
