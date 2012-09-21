5aa34691ec721a07c687aa3b12ee3af2dabfec12
Iván Ibarra <ivanfc0o@gmail.com>
2012-09-20 22:01:50 -0700

    Fixed problem with IE
    Fixed problem with IE when it calls json 'without padding'

--- jquery.facebookID.js
+++ jquery.facebookID.js
@@ -1,26 +1,25 @@
 /*
- *  Project: jquery.facebookID
+ *  Project: facebookID
  *  Description: Get basic info from an user without Apikey or App
  *  Author: @ivanfc0o
  */
 
 ;(function ( $, window, undefined ) {
     
-// Plugin name
 var pluginName = 'facebookID';
-
     // plugin __constructor
     var plugin = function(user, avatar, callback){
         var avatar = avatar||[50,50];
         var emailRegExp = /^\w[\+\.\w-]*@facebook.com$/i,
               user = emailRegExp.test(user)?user.split("@")[0]:user,
               result = {},
-              data = $.getJSON("https://graph.facebook.com/"+user, null, function(d){
+              data = $.getJSON("https://graph.facebook.com/"+user+"?callback=?", function(d){
                                       result.name = d.name;
+                                      result.status = d.name?true:false;
                                       result.avatar = "http://graph.facebook.com/"+d.username+"/picture?width="+avatar[0]+"&height="+avatar[1];
                                       result.url = d.link||"http://fb.com/"+d.username;
-                                      if((typeof callback)==="function"){ callback(result); }
-                          });
+                                      if((typeof callback)==="function"){  callback(result); }
+               });
               return result;
     };
 
